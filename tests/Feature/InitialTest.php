7edafbe3fa47e423a86eb300793734c9a6c8f910
Kevin Cifuentes <cifuentesk85@gmail.com>
2019-09-11 11:30:02 -0500

    Configuraciones iniciales para Tests
    Se realizan las configuraciones iniciales para la realización de Testing en el Package.

---
+++ tests/Feature/InitialTest.php
@@ -0,0 +1,11 @@
+<?php
+namespace talentui33\ActiveCampaign\Tests;
+
+use Orchestra\Testbench\TestCase;
+
+class InitialTest extends TestCase{
+  public function testInitial()
+  {
+    $this->assertTrue(true);
+  }
+}
