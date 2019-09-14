66229c30c3aeddd622c76ac6385aa2b2444e2247
Kevin Cifuentes <cifuentesk85@gmail.com>
2019-09-14 17:39:03 -0500

    Pruebas para Contactos
    Se finaliza el crud para los contactos y se realizan los test para cada uno de los metodos.

--- src/Contact.php
+++ src/Contact.php
@@ -4,9 +4,12 @@ namespace TalentuI33\ActiveCampaign;
 
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\Psr7\Response;
 
 class Contact extends ActiveCampaign
 {
+    protected static $url = 'contacts';
+
     public function __construct()
     {
         parent::__construct();
@@ -17,7 +20,32 @@ class Contact extends ActiveCampaign
         parent::__construct();
     }
 
-    public static function getAll(): string
+    public static function add(string $first_name, string $last_name, string $email, string $phone): Response
+    {
+        $response = null;
+        try {
+            if (!self::$client instanceof Client) {
+                (new Contact)->init();
+            }
+
+            $response = self::$client->request('POST', static::$url, [
+                'json' => [
+                    "contact" => [
+                        "email" => $email,
+                        "firstName" => $first_name,
+                        "lastName" => $last_name,
+                        "phone" => $phone
+                    ]
+                ]
+            ]);
+        } catch (GuzzleException $e) {
+            abort($e->getCode(), $e->getMessage());
+        }
+
+        return $response;
+    }
+
+    public static function update(string $id, string $first_name = null, string $last_name = null, string $email = null, string $phone = null): Response
     {
         $response = null;
 
@@ -25,11 +53,83 @@ class Contact extends ActiveCampaign
             if (!self::$client instanceof Client) {
                 (new Contact)->init();
             }
-            $response = self::$client->request('GET', 'contacts');
+
+            $data = collect();
+
+            if (!empty($first_name)) {
+                $data->put("firstName", $first_name);
+            }
+
+            if (!empty($last_name)) {
+                $data->put("lastName", $last_name);
+            }
+
+            if (!empty($email)) {
+                $data->put("email", $email);
+            }
+
+            if (!empty($phone)) {
+                $data->put("phone", $phone);
+            }
+
+            $jsonData = ['contact' => $data->toArray()];
+
+            $response = self::$client->request('PUT', static::$url . "/{$id}", [
+                'json' => $jsonData
+            ]);
         } catch (GuzzleException $e) {
             abort($e->getCode(), $e->getMessage());
         }
 
-        return (string)$response->getBody();
+        return $response;
+    }
+
+    public static function delete(string $id)
+    {
+        $response = null;
+        try {
+            if (!self::$client instanceof Client) {
+                (new Contact)->init();
+            }
+
+            $response = self::$client->request('DELETE', static::$url . "/{$id}");
+        } catch (GuzzleException $e) {
+            abort($e->getCode(), $e->getMessage());
+        }
+
+        return $response;
+    }
+
+    public static function getAll(): Response
+    {
+        $response = null;
+
+        try {
+            if (!self::$client instanceof Client) {
+                (new Contact)->init();
+            }
+
+            $response = self::$client->request('GET', static::$url);
+        } catch (GuzzleException $e) {
+            abort($e->getCode(), $e->getMessage());
+        }
+
+        return $response;
+    }
+
+    public static function getByEmail(string $email): Response
+    {
+        $response = null;
+        try {
+            if (!self::$client instanceof Client) {
+                (new Contact)->init();
+            }
+
+            $response = self::$client->request('GET', static::$url . "?email={$email}");
+        } catch (GuzzleException $e) {
+            abort($e->getCode(), $e->getMessage());
+        }
+
+        return $response;
     }
 }
--- tests/Unit/ContactTest.php
+++ tests/Unit/ContactTest.php
@@ -8,9 +8,67 @@ use Tests\TestCase;
 
 class ContactTest extends TestCase
 {
+    protected $contactId;
+
     public function testGetAllContacts(): void
     {
-        $contacts = Contact::getAll();
-        $this->assertJson($contacts);
+        $response = Contact::getAll();
+
+        $this->assertTrue($response->getStatusCode() === 200);
+    }
+
+    public function testGetContactByEmail()
+    {
+        $response = Contact::getByEmail('smilenaferr22@gmail.com');
+
+        $this->assertTrue($response->getStatusCode() === 200);
+    }
+
+    public function testAddNewContact(): void
+    {
+        $response = Contact::add(
+            'Sandra Milena',
+            'Ferreira Dimian',
+            'smilenaferr22@gmail.com',
+            '3154770365'
+        );
+
+        $contact = json_decode((string)$response->getBody());
+        $this->contactId = $contact->contact->id;
+
+        $this->assertTrue($response->getStatusCode() === 201);
+    }
+
+    public function testUpdateContact()
+    {
+        $response = Contact::getByEmail('smilenaferr22@gmail.com');
+        $contact = json_decode((string)$response->getBody());
+
+        if (isset($contact->contacts[0])) {
+            $response = Contact::update(
+                $contact->contacts[0]->id,
+                'Sandra',
+                'Ferreira',
+                'smilenaferr22@gmail.com',
+                '3154770365'
+            );
+
+            $this->assertTrue($response->getStatusCode() === 200);
+        } else {
+            $this->assertTrue(false, 'Data not Found on API');
+        }
+    }
+
+    public function testDeleteContact()
+    {
+        $response = Contact::getByEmail('smilenaferr22@gmail.com');
+        $contact = json_decode((string)$response->getBody());
+
+        if (isset($contact->contacts[0])) {
+            $response = Contact::delete($contact->contacts[0]->id);
+            $this->assertTrue($response->getStatusCode() === 200);
+        } else {
+            $this->assertTrue(false, 'Data not Found on API');
+        }
     }
 }
