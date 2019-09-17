27b5ae507cb6fe9ddd236724bab6960d3054f584
Kevin Cifuentes <cifuentesk85@gmail.com>
2019-09-17 13:07:11 -0500

    Se modifica el Nombre del archivi ServiceProvider por error de conflicto por ActiveCampaignServiceProvider

---
+++ src/ActiveCampaignServiceProvider.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace TalentuI33\ActiveCampaign;
+
+
+use Illuminate\Foundation\Application as LaravelApplication;
+use Illuminate\Support\ServiceProvider;
+
+class ActiveCampaignServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    public function boot(): void
+    {
+        $this->setupConfig();
+    }
+
+    protected function setupConfig()
+    {
+        $source = realpath(__DIR__ . '/../config/activecampaign.php');
+        if ($this->app instanceof LaravelApplication && $this->app->runningInConsole()) {
+            $this->publishes([$source => config_path('activecampaign.php')]);
+        }
+
+        $this->mergeConfigFrom($source, 'activecampaign');
+    }
+
+    public function register(): void
+    {
+        $this->mergeConfigFrom(
+            __DIR__ . '/../config/activecampaign.php', 'activecampaign'
+        );
+
+        $this->app->bind('activecampaign', function ($app) {
+            return new ActiveCampaign();
+        });
+    }
+}
